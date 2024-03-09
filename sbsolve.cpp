2896eae6f7380c62ba2158349d77c6e53eaa60ae
Taylor <taylor.foster23@gmail.com>
2024-03-08 17:11:13 -0800

    update ui

--- sbsolve.cpp
+++ sbsolve.cpp
@@ -3,37 +3,55 @@
 using namespace std;
 
 void processInput(string& input) {
+    for (char& c : input) {
+        c = tolower(c);
+    }
+
     input.erase(remove_if(input.begin(), input.end(), [](char c) {
         return c < 'a' || c > 'z';
     }), input.end());
 }
 
 int main(int argc, char* argv[]) {
-    chrono::time_point<chrono::_V2::system_clock> start, end;
-    chrono::duration<float> duration;
-    float ms;
-
     Solver solver;
-    
-    string optionalChars;
-    string requiredChars;
-    cout << "Input optional characters: ";
-    getline(cin, optionalChars);
-    cout << "Input required characters: ";
-    getline(cin, requiredChars);
+    bool loop = true;
 
-    processInput(optionalChars);
-    processInput(requiredChars);
+    while (loop) {
+        chrono::time_point<chrono::_V2::system_clock> start, end;
+        chrono::duration<float> duration;
+        float ms;
 
-    start = chrono::high_resolution_clock::now();
-    vector<string> results = solver.solve(optionalChars, requiredChars);
-    end = chrono::high_resolution_clock::now();
+        string optionalChars;
+        string requiredChars;
+        cout << "Input optional characters: ";
+        getline(cin, optionalChars);
+        cout << "Input required characters: ";
+        getline(cin, requiredChars);
+        processInput(optionalChars);
+        processInput(requiredChars);
 
-    duration = end - start;
-    ms = duration.count() * 1000.0f;
-    cout << "Duration (ms): " << ms << "\n";
+        start = chrono::high_resolution_clock::now();
+        vector<string> results = solver.solve(optionalChars, requiredChars);
+        end = chrono::high_resolution_clock::now();
 
-    sort(results.begin(), results.end());
+        duration = end - start;
+        ms = duration.count() * 1000.0f;
+        cout << "Duration (ms): " << ms << "\n";
+
+        sort(results.begin(), results.end());
+
+        for (auto& result : results) {
+            cout << result << endl;
+        }
+
+        string in;
+        cout << "Go again? <y/n>: ";
+        getline(cin, in);
+        processInput(in);
+        if (in != "y") {
+            loop = false;
+        } 
+    }
 
     return 0;
 }
